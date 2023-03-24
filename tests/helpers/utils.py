7d9eb1ab82059fab04cc60a503c9b5630c1fda6e
Dave O <davidobrite@gmail.com>
2023-03-24 10:04:51 -0700

    Create an ImmediateBpm for testing
    There is no time component to this bpm. It triggers on each update.

--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -1,6 +1,23 @@
+from froggytv.bpm import Bpm
+from froggytv.triggers import Noop
+
+
 class CountingTriggerable():
     def __init__(self):
         self.count = 0
 
     def trigger(self, _tick):
         self.count += 1
+
+
+class ImmediateBPM():
+    def __init__(self, resolution=Bpm.DEFAULT_RESOLUTION):
+        self.resolution = resolution
+        self._tick = 0
+
+    def update(self, _now, triggerable=Noop()):
+        self._tick += 1
+        if self._tick == self.resolution:
+            self._tick = 0
+
+        triggerable.trigger(self._tick)
