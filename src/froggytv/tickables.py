779ffcab8a5ec73d4f023e8750aa1aba03c491bf
Dave O <davidobrite@gmail.com>
2023-03-24 19:30:27 -0700

    chore(callables): unify callables and tickables

--- src/froggytv/tickables.py
+++ src/froggytv/tickables.py
@@ -2,17 +2,24 @@ class Noop:
     def __init__(self):
         pass
 
+    def __call__(self, __tick__):
+        pass
+
     def tick(self, __ticks__):
         pass
 
 
 class FanOut:
-    def __init__(self, tickables):
-        self._tickables = tickables
+    def __init__(self, collection):
+        self._collection = collection
+
+    def __call__(self, tick):
+        for elem in self._collection:
+            elem(tick)
 
     def tick(self, ticks):
-        for tickable in self._tickables:
-            tickable.tick(ticks)
+        for elem in self._collection:
+            elem.tick(ticks)
 
 
 class Division:
@@ -47,3 +54,36 @@ class TicksToCall:
 
         if self._count == self._ticks_to_call:
             self._count = 0
+
+
+class Periodic:
+    def __init__(self, resolution, callable, mult=1, pwm=0.5):
+        self._resolution = resolution
+        self._callable = callable
+        self._mult = mult
+        self._next_mult = None
+        self._pwm = pwm
+        self._count = 0
+
+    def tick(self, tick):
+        if self._count == 0:
+            self._callable(tick)
+
+        self._count += 1
+
+        if not self._count == self._call_count():
+            return
+
+        self._count = 0
+
+        if not (self._next_mult and tick == 0):
+            return
+
+        self._mult = self._next_mult
+        self._next_mult = None
+
+    def set_mult(self, mult):
+        self._next_mult = mult
+
+    def _call_count(self):
+        return self._resolution / self._mult / (1 / self._pwm)
--- tests/test_tickables.py
+++ tests/test_tickables.py
@@ -1,11 +1,14 @@
 import froggytv.tickables as tickables
-from helpers.utils import CountingTickable, ImmediateBPM, FakeOutput
+from helpers.utils import CountingTickable, ImmediateBPM, FakeOutput, is_even
 import pytest
 
 
 class TestNoop:
-    def test_noop_does_nothing(self):
+    def test_ticking_noop_does_nothing(self):
         assert tickables.Noop().tick(0) is None
+
+    def test_calling_noop_does_nothing(self):
+        assert tickables.Noop()(0) is None
 
 
 class TestDivision:
@@ -61,3 +64,70 @@ class TestTicksToCall:
             bpm.update(now, ticks_to_call)
 
         assert callable.count == expected_calls
+
+
+class TestPeriodic:
+    @pytest.fixture
+    def callable(self):
+        return FakeOutput()
+
+    @pytest.mark.parametrize(
+        "resolution, mult, call_count, expected_count",
+        [
+            (640, 1, 0, 0),
+            (640, 1, 1, 1),
+            (640, 1, 640, 2),
+            (640, 1, 641, 3),
+            (640, 2, 640, 4),
+            (640, 2, 641, 5),
+            (640, 2, 960, 6),
+            (640, 2, 961, 7),
+            (640, 0.5, 1280, 2),
+            (640, 0.5, 1281, 3),
+        ],
+    )
+    def test_periodic(
+        self,
+        callable,
+        resolution,
+        mult,
+        call_count,
+        expected_count,
+    ):
+        now = None
+        bpm = ImmediateBPM(resolution)
+        periodic = tickables.Periodic(bpm.resolution, callable, mult=mult)
+
+        for _ in range(call_count):
+            bpm.update(now, periodic)
+
+        assert callable.count == expected_count
+        assert callable.on == is_even(expected_count)
+
+    @pytest.mark.parametrize(
+        "resolution, pwm, call_count, expected_on",
+        [
+            (640, 0.5, 0, False),
+            (640, 0.5, 1, True),
+            (640, 0.5, 320, True),
+            (640, 0.5, 321, False),
+            (640, 0.5, 640, False),
+            (640, 0.5, 641, True),
+        ],
+    )
+    def test_pwm(
+        self,
+        callable,
+        resolution,
+        pwm,
+        call_count,
+        expected_on,
+    ):
+        now = None
+        bpm = ImmediateBPM(resolution)
+        periodic = tickables.Periodic(bpm.resolution, callable, pwm=pwm)
+
+        for _ in range(call_count):
+            bpm.update(now, periodic)
+
+        assert callable.on == expected_on
